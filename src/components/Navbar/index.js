8a922c03d44b15ef285e2c875564ec3ed125c067
Aylin Görgün <aylingrgn2@gmail.com>
2023-02-11 17:55:55 +0300

    kurum giris link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className='flex justify-end m-10'>
-        <p className="text-center text-pink-600 font-bold">Kurum Girişi</p>
+    <div className="flex justify-end m-10">
+      <Link
+        href={"/stk/login"}
+        className="text-center text-pink-600 font-bold"
+      >Kurum Girişi</Link>
     </div>
-  )
+  );
 }
