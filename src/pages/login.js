31c35798dd16e69d5853988b19ce8e5b4026d7f9
aylin.gorgun <aylingrgn2@gmail.com>
2023-02-13 16:09:13 +0300

    formik added

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,6 +5,8 @@ import { UserContext } from "@/src/context/UserContext";
 import { app } from "@/src/firebase-config";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
+import { Formik } from "formik";
+
 import toast from "react-hot-toast";
 
 const auth = getAuth(app);
@@ -37,33 +39,64 @@ export default function Login() {
 
   return (
     <div className="m-10 lg:mx-36 space-y-10">
-      <form className="max-w-xl mx-auto grid gap-y-4" onSubmit={handleSubmit}>
-        <p className="text-4xl font-bold">Kurum Girişi</p>
-        <p className="text-gray-500 text-xs py-2" >Tüm alanlar doldurulmalıdır.</p>
-        <Input
-          label="E-mail"
-          name="name"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <Input
-          type="password"
-          label="Şifre"
-          name="name"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <Button
-          disabled={!email || !password}
-          className="w-36 right-0 my-5"
-          type="submit"
-          color="gray"
-        >
-          Giriş yap
-        </Button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Geçerli bir e-mail addresi girin.";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit;
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ errors, touched, handleSubmit }) => (
+          <form
+            className="max-w-xl mx-auto grid gap-y-4"
+            onSubmit={handleSubmit}
+          >
+            <p className="text-4xl font-bold">Kurum Girişi</p>
+            <p className="text-gray-500 text-xs py-2">
+              Tüm alanlar doldurulmalıdır.
+            </p>
+            {errors.email && touched.email && errors.email}
+            <Input
+              label="E-mail"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && touched.email && errors.email}
+            <Input
+              type="password"
+              label="Şifre"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && touched.password && errors.password}
+            <Button
+              disabled={!email || !password}
+              className="w-36 right-0 my-5"
+              type="submit"
+              color="gray"
+            >
+              Giriş yap
+            </Button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
